fca5d475fb332b1f346ac8cd07024480a4b1e967
puresummersky <694645381@qq.com>
2022-01-04 01:58:21 +0800

    config_file

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
-
+    return render_template('mybase.html')
 
 if __name__ == '__main__':
     app.run()
